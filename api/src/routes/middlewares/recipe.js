11b8ef355a55468ed9d366fbbed43ad12e90a475
anthony29p <anthony.perez@uni.pe>
2022-09-16 17:56:01 -0500

    Update Back-end (own DB)

--- api/src/routes/middlewares/recipe.js
+++ api/src/routes/middlewares/recipe.js
@@ -3,77 +3,21 @@ const { Recipe, Diet } = require('../../db');
 const router = Router();
 
 
-const { getAllRecipes  } = require('../../controllers')
+const { saveInfo, getRecipes, getRecipesByID, postRecipe } = require('../../controllers')
+
+// Insert data in the DB -- use once
+router.post('/test', saveInfo)
 
 // Gives everything in the DB
 
-router.get('/', async (req,res) =>{
-    const {name} = req.query;
-
-    const recetas = await getAllRecipes();
-    try{
-        if(name){
-            const recetasFiltradas = recetas.filter(receta => receta.name.toLowerCase().includes(name.toLowerCase()))
-            
-            recetasFiltradas.length>0?res.status(200).send(recetasFiltradas):res.status(404).send(`Recipes with the word ${name} were not found. Would you like to try another?`)
-        }
-        else{
-            res.send(recetas)
-        }
-    }
-    catch(error){
-        res.status(404).send(error)
-    }
-})
+router.get('/',getRecipes )
 
 // Uncomplete task
 
-router.get('/:idReceta', async (req,res) =>{
-    const {idReceta} = req.params || {}
-
-    const recetas = await getAllRecipes();
-    try{
-        if(idReceta){
-            const recetasFiltradas = await recetas.filter(receta => receta.id == idReceta)
-            
-            recetasFiltradas.length?
-            res.status(200).send(recetasFiltradas):
-            res.status(404).send('Recipe was not found')
-        }
-    }
-    catch(error){
-        res.status(404).send(error)
-    }
-})
+router.get('/:idReceta', getRecipesByID)
 
 // Saves the recipes in the DB
-router.post('/', async (req,res) =>{
-    const { name,description,healthScore,howToDo,image,diet } = req.body;
-
-    try{
-        if(name && description){
-            const receta = await Recipe.create({ name,description,healthScore,howToDo,image })
-            
-            const dieta = await Diet.findAll({
-                where:{
-                    name: diet
-                }
-            })
-            
-            await receta.addDiet(dieta)
-            // console.log(dieta)
-            return res.status(200).send("Recipe created successfully")
-        }
-        else{
-            return res.status(400).send("Data received is not enough")
-        }
-    }
-    catch(error){
-        console.log(error);
-        res.status(400).send("Hola soy el error del POST")
-        
-    }
-})
+router.post('/',postRecipe)
 
 
 module.exports = router;
