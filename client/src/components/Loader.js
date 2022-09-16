949be3345facef6f8d3ade0399f2b931ab3b7b97
anthony29p <anthony.perez@uni.pe>
2022-09-15 23:07:30 -0500

    Loaders

---
+++ client/src/components/Loader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './styles/Loader.css';
+
+export default function Loader (){
+
+    return(
+        <div class="wrapper">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
+        </div>
+    )
+}
