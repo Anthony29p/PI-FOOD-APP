fcf45a39575a8357274430e3666fc152ebebdf3e
anthony29p <anthony.perez@uni.pe>
2022-09-16 10:32:40 -0500

    PORT fix

--- api/index.js
+++ api/index.js
@@ -20,13 +20,15 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-
+const {
+  PORT ,
+} = process.env;
 
 
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
